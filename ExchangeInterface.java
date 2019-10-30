c52dcf6fb4c6a43a0e15720b87fd5eb80d913ea3
odinzero <alekseykravchenko1979@gmail.com>
2019-10-30 18:54:33 +0200

    Delete ExchangeInterface.java

--- ExchangeInterface.java
+++
@@ -1,9 +0,0 @@
-
-package exchangetask;
-
-public interface ExchangeInterface {
-
-    public void send(long orderId, boolean isBuy, int price, int size) throws RequestRejectedException;
-
-    public void cancel(long orderId) throws RequestRejectedException;
-}
